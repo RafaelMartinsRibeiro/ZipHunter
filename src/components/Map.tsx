13e4e6eb828e154ccc178de060045dab7d568d41
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-23 21:46:30 -0300

    Added Map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,16 @@
+import Chart from "react-google-charts";
+import { data, options } from "../data";
+
+export const Map = () => {
+  return (
+    <div>
+      <Chart
+        chartType="GeoChart"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+      />
+    </div>
+  );
+};
