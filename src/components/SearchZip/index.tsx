27801a23b4465df5b53dee786668627ce2cc86bd
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-25 21:33:06 -0300

    Added SearchZip component

---
+++ src/components/SearchZip/index.tsx
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+
+import { GiFlyingTarget } from "react-icons/gi";
+import { FaSearchLocation } from "react-icons/fa";
+
+import "./styles.scss";
+import { ZipContext } from "../../contexts/ZipContext";
+
+export const SearchZip = () => {
+  const { zipCode, handleZipCodeChange } = useContext(ZipContext);
+
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="title">
+          <span>Zip</span>Hunter
+        </div>
+        <GiFlyingTarget />
+      </header>
+
+      <main>
+        <div className="formContainer">
+          <form className="form">
+            <h1>Procure e encontre zipS de todo o Brasil</h1>
+            <span>
+              O ZipHunter ajuda você a encontrar um CEP e tudo que esteja
+              relacionado a ele.
+            </span>
+
+            <div className="searchBar">
+              <input
+                type="text"
+                value={zipCode}
+                placeholder="Informe o CEP"
+                onChange={handleZipCodeChange}
+              />
+              <span>
+                <FaSearchLocation />
+              </span>
+            </div>
+          </form>
+        </div>
+
+        {/* <Map /> */}
+      </main>
+    </div>
+  );
+};
