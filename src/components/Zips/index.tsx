6452bf1fd9856cd236665fa629b6287691a446ee
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-29 14:52:37 -0300

    Added Zips responsive layout

--- src/components/Zips/index.tsx
+++ src/components/Zips/index.tsx
@@ -1,10 +1,14 @@
-import { CepTable } from "../CepTable";
+import { CepTable } from "./CepTable";
 import { Map } from "./Map";
+
+import "./styles.scss"
 
 export const Zips = () => {
   return (
     <div className="stateZips">
-      <h3>Faixas de CEPS do Brasil</h3>
+      <div>
+        <h3>Faixas de CEPS do Brasil</h3>
+      </div>
       <Map />
       <CepTable />
     </div>
